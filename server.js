a2f33f8412ac2ced780fc7dded5db4397f1b13a5
nalalalan <pham.alann@gmail.com>
2026-04-27 16:48:29 -0400

    Serve Ocean preview image as PNG

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const mimeTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
   ".svg": "image/svg+xml",
   ".ico": "image/x-icon",
 };
